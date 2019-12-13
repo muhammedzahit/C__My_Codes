a67172e3ae3fb118ba9784bfcb017ddaceb8136d
Muhammed Zahid <muhammetzahitaydin@gmail.com>
2019-12-13 14:34:28 +0300

    commit

---
+++ cmake-build-debug/CMakeFiles/C__My_Codes.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C__My_Codes.dir/main.c.obj"
+  "C__My_Codes.exe"
+  "C__My_Codes.exe.manifest"
+  "C__My_Codes.pdb"
+  "libC__My_Codes.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/C__My_Codes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/C__My_Codes.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for C__My_Codes.
+# This may be replaced when dependencies are built.
